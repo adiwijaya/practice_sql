fc94f25afb54dd3d43f5c1a3d793650488e8f4c0
root <adiwijaya25@gmail.com>
2019-03-27 11:27:06 +0000

    change directory password

--- ELT.sh
+++ ELT.sh
@@ -1,8 +1,8 @@
 #EXPORT
-sudo -u iykra_trainee PGPASSFILE=/home/iykra_trainee/sql/.pgpass_sandbox psql -U iykra_trainee -d sandbox -a -f export_table.sql
+sudo -u iykra_trainee PGPASSFILE=/srv/.pgpass_sandbox psql -U iykra_trainee -d sandbox -a -f export_table.sql
 
 #IMPORT 
-sudo -u iykra_trainee PGPASSFILE=/home/iykra_trainee/sql/.pgpass psql -U iykra_trainee -d sandbox_target -a -f import_table.sql
+sudo -u iykra_trainee PGPASSFILE=/srv/.pgpass psql -U iykra_trainee -d sandbox_target -a -f import_table.sql
 
 #TRANSFORM
-sudo -u iykra_trainee PGPASSFILE=/home/iykra_trainee/sql/.pgpass psql -U iykra_trainee -d sandbox_target -a -f aggregate_sql.sql
+sudo -u iykra_trainee PGPASSFILE=/srv/.pgpass psql -U iykra_trainee -d sandbox_target -a -f aggregate_sql.sql
