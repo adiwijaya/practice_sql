d1f4da8c0a657f27dd739411ce3e4686a0f025ad
root <adiwijaya25@gmail.com>
2019-03-27 10:31:37 +0000

    add import export

---
+++ export_table.sql
@@ -0,0 +1,1 @@
+\COPY procedure_details TO '/home/iykra_trainee/data/procedure_details.csv' DELIMITER ',' CSV HEADER;
---
+++ import_table.sql
@@ -0,0 +1,1 @@
+\COPY procedure_details FROM '/home/iykra_trainee/data/procedure_details.csv' DELIMITERS ',' CSV HEADER;
